0f73604b677e4c84a94afaedae2d0b77be78d891
vadim11041996 <vadim11041996@hotmail.com>
2018-06-10 20:54:20 +0300

    image processed with jimp

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const jimp = require('jimp');
 
 const config = require('./config/config');
 
 const app = express();
+
+//Middleware
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+jimp.read("img/lena.jpg", function (err, lenna) {
+    if (err) throw err;
+    lenna.resize(256, 256)            // resize
+        .quality(60)                 // set JPEG quality
+        .greyscale()                 // set greyscale
+        .write("lena-small-bw.jpg"); // save
+});
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.listen(config.port,()=>{
     console.log('Server started on port :' + config.port);
